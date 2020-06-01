2be2c77c961ad660c03b47c6da747869a37e0e36
albert <albertkingdom@gmail.com>
2020-06-01 19:51:45 +0800

    update Payprogressbar and its css

--- src/components/shop/PayProgessbar.js
+++ src/components/shop/PayProgessbar.js
@@ -1,42 +1,21 @@
 import React from 'react'
 import { withRouter } from 'react-router-dom'
+import styles from './PayProgressbar.module.scss'
 function PayProgressbar(props) {
   console.log('payprogressbar', props)
   return (
     <>
-      <div
-        className="d-flex justify-content-center align-items-center"
-        style={{ paddingTop: '50px' }}
-      >
-        <div
-          className={
-            props.location.pathname == '/cart_new'
-              ? 's-circleActive'
-              : 's-circle'
-          }
-        >
-          <h3>1</h3>
-          {/* <h6>檢視購物車</h6> */}
-        </div>
-        <div className="s-line"></div>
-        <div
-          className={
-            props.location.pathname == '/payment'
-              ? 's-circleActive'
-              : 's-circle'
-          }
-        >
-          <h3>2</h3>
-        </div>
-        <div className="s-line"></div>
-        <div
-          className={
-            props.location.pathname == '/order' ? 's-circleActive' : 's-circle'
-          }
-        >
-          <h3>3</h3>
-        </div>
-      </div>
+      <ul className={`${styles.newprogressbar}`}>
+        <li className={
+          props.location.pathname == '/cart_new'?`${styles.active}`:''
+        }>1<span>購物車</span></li>
+        <li className={
+          props.location.pathname == '/payment'?`${styles.active}`:''
+        }>2<span>付款方式</span></li>
+        <li className={
+          props.location.pathname == '/order'?`${styles.active}`:''
+        }>3<span>訂單成立</span></li>
+      </ul>
     </>
   )
 }
